0801a08d64b6d50014d575a63409eaffe3bb6bf9
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-08-18 17:52:38 -0300

    comentando envio de email de desen

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'dotget',
-  :password => 'dotget12',
-  :domain => 'dotget.com.br',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'dotget',
+#   :password => 'dotget12',
+#   :domain => 'dotget.com.br',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }

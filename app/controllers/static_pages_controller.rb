00b20318c7147817f42753951968e37d421dd46e
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-08-18 23:34:41 -0300

    alteração pagina princial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @clientes =  Clienteempresa.where(empresa_id: current_user.empresa_id).count
+      @premios = Premio.where(empresa_id: current_user.empresa_id).count
     end
   end
 end

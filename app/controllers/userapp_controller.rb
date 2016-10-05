9cd75a7dbe7ea2fa5a62a41ea6cab03a2db9db2c
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-10-05 18:52:00 -0300

    alterações appuser

--- app/controllers/userapp_controller.rb
+++ app/controllers/userapp_controller.rb
@@ -3,9 +3,11 @@ class UserappController < ApplicationController
   def login
     resource = User.find_for_database_authentication(email: params[:email])
 
-    return invalid_login_attempt unless resource
-    if resource.valid_password?(params[:password])
+    if resource == nil
+      return render :json => false
+    end
 
+    if resource.valid_password?(params[:password])
       #user = User.find(resource.id)
       json_resource = {:id => resource.id,
                        :nome =>  resource.pessoa.nome_primeiro,
